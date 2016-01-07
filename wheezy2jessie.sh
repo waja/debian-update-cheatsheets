af86c7e97691840929f809c0d0789fb52e4a2758
Jan Wagner <waja@cyconet.org>
2016-01-07 18:18:28 +0100

    Adding comment about postgres upgrade

--- wheezy2jessie.sh
+++ wheezy2jessie.sh
@@ -124,6 +124,9 @@ sed -i "s/adapter: mysql/adapter: mysql2/" /etc/redmine/default/database.yml
 # Fixing Typo bug in claav-daemon (http://bugs.debian.org/778507)
 sed -i "s/DEBCONFILE/DEBCONFFILE/" /var/lib/dpkg/info/clamav-daemon.postinst
 
+# Upgrade postgres
+if [ "$(dpkg -l | grep "postgresql-9.1" | awk {'print $2'})" = "postgresql-9.1" ]; then aptitude install postgresql-9.4 && pg_dropcluster --stop 9.4 main && /etc/init.d/postgresql stop && pg_upgradecluster -v 9.4 9.1 main && pg_dropcluster 9.1 main; fi
+
 # remove old squeeze packages left around (keep eyes open!)
 apt-get autoremove
 aptitude search ?obsolete
