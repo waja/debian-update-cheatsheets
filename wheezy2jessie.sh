8e1c6e4cae2a5ddac4219422a2c2aa475f1e7dca
Jan Wagner <waja@cyconet.org>
2017-07-31 21:30:02 +0200

    Don't remove check-openmanage

--- wheezy2jessie.sh
+++ wheezy2jessie.sh
@@ -166,8 +166,8 @@ pg_dropcluster 9.1 main
 
 # remove old squeeze packages left around (keep eyes open!)
 apt-get autoremove && \
-apt purge $(aptitude search ?obsolete | grep -v linux-image | awk '/^i *A/ { print $3 }') && \
-apt purge $(aptitude search ?obsolete | grep -v linux-image | awk '/^i/ { print $2 }') && \
+apt purge $(aptitude search ?obsolete | grep -v -E 'linux-image|check-openmanage' | awk '/^i *A/ { print $3 }') && \
+apt purge $(aptitude search ?obsolete | grep -v -E 'linux-image|check-openmanage' | awk '/^i/ { print $2 }') && \
 apt purge $(dpkg -l | grep etch | grep -v xen | grep -v unbound | grep -v finch | awk '/^rc/ { print $2 }') && \
 apt purge $(dpkg -l | grep lenny | grep -v xen | awk '/^rc/ { print $2 }') && \
 apt purge $(dpkg -l | grep -E 'deb6|squeeze' | grep -v xen | awk '/^rc/ { print $2 }') && \
