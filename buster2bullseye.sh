5e433f1c64f5da03fcafc699e185bf454710b414
Jan Wagner <waja@cyconet.org>
2021-02-07 17:51:19 +0100

    First bullseye version

--- buster2bullseye.sh
+++ buster2bullseye.sh
@@ -1,21 +1,18 @@
-Please also refer to http://www.debian.org/releases/buster/releasenotes and use your brain! If you can’t figure out what one of the commands below does, this is not for you. Expert mode only :)
+Please also refer to http://www.debian.org/releases/bullseye/releasenotes and use your brain! If you can’t figure out what one of the commands below does, this is not for you. Expert mode only :)
 
 # Crossgrading ?!?
-[ "$(dpkg --print-architecture)" == "i386" ] && echo "How about crossgrading to amd64 as described in https://stbuehler.de/blog/article/2017/06/28/debian_stretch__upgrade_32-bit_to_64-bit.html?"
+[ "$(dpkg --print-architecture)" == "i386" ] && echo "How about crossgrading to amd64 as described in https://stbuehler.de/blog/article/2017/06/28/debian_buster__upgrade_32-bit_to_64-bit.html?"
 
-# Since running buster with older kernels may not work well (eg jessie kernels) lets see if we have a domU without own kernel
-if [ -d /sys/module/xen_netfront -a $(dpkg -l | grep grub-xen-bin | wc -l) = 0 -a $(dpkg -l | grep linux-image | wc -l) = 0 ]; then echo "Please run 'apt install grub-xen-bin grub-xen linux-image-amd64 && update-grub' to install a recent kernel and boot your domU with it next time"; fi
-
-# upgrade to UTF-8 locales (http://www.debian.org/releases/buster/amd64/release-notes/ap-old-stuff.en.html#switch-utf8)
+# upgrade to UTF-8 locales (http://www.debian.org/releases/bullseye/amd64/release-notes/ap-old-stuff.en.html#switch-utf8)
 dpkg-reconfigure locales
 
 # remove unused config file
 rm -rf /etc/network/options /etc/environment
 
-# are there 3rd party packages installed? (https://www.debian.org/releases/buster/amd64/release-notes/ch-upgrading.de.html#system-status)
+# are there 3rd party packages installed? (https://www.debian.org/releases/bullseye/amd64/release-notes/ch-upgrading.de.html#system-status)
 aptitude search '~i(!~ODebian)'
 
-# check for ftp protocol in sources lists (https://www.debian.org/releases/buster/amd64/release-notes/ch-information.en.html#deprecation-of-ftp-apt-mirrors)
+# check for ftp protocol in sources lists (https://www.debian.org/releases/bullseye/amd64/release-notes/ch-information.en.html#deprecation-of-ftp-apt-mirrors)
 rgrep --color "deb ftp" /etc/apt/sources.list*
 
 # Transition and remove entries from older releases
@@ -27,12 +24,12 @@ sed -i /jessie/d /etc/apt/sources.list*
 sed -i /volatile/d /etc/apt/sources.list*
 sed -i /proposed-updates/d /etc/apt/sources.list*
 # change distro (please move 3rd party sources to /etc/apt/sources.list.d/), maybe look into http://ftp.cyconet.org/debian/sources.list.d/
-sed -i s/stretch/buster/g /etc/apt/sources.list*
-sed -i "s/ stable/ buster/g" /etc/apt/sources.list*
-sed -i s/stretch/buster/g /etc/apt/preferences*
-sed -i s/stretch/buster/g /etc/apt/sources.list.d/*stretch*
-rename s/stretch/buster/g /etc/apt/sources.list.d/*stretch*
-rgrep --color stretch /etc/apt/sources.list*
+sed -i s/buster/bullseye/g /etc/apt/sources.list*
+sed -i "s/ stable/ bullseye/g" /etc/apt/sources.list*
+sed -i s/buster/bullseye/g /etc/apt/preferences*
+sed -i s/buster/bullseye/g /etc/apt/sources.list.d/*buster*
+rename s/buster/bullseye/g /etc/apt/sources.list.d/*buster*
+rgrep --color buster /etc/apt/sources.list*
 apt-get update
 
 # check package status
@@ -49,15 +46,15 @@ aptitude unmarkauto $(dpkg-query -W 'linux-image-4.9.0*' | cut -f1)
 apt-get -o APT::Get::Trivial-Only=true dist-upgrade || df -h
 
 # record session
-script -t 2>~/upgrade-buster.time -a ~/upgrade-buster.script
+script -t 2>~/upgrade-bullseye.time -a ~/upgrade-bullseye.script
 
 # install our preseed so libc doesn't whine
-cat > /tmp/stretch.preseed <<EOF
+cat > /tmp/buster.preseed <<EOF
 libc6 glibc/upgrade boolean true
 libc6 glibc/restart-services string
 libc6 libraries/restart-without-asking boolean true
 EOF
-/usr/bin/debconf-set-selections /tmp/stretch.preseed
+/usr/bin/debconf-set-selections /tmp/buster.preseed
 
 # update aptitude first
 [ "$(which aptitude)" = "/usr/bin/aptitude" ] && aptitude install aptitude && \
@@ -95,7 +92,6 @@ sed -i '/codename=..distro_codename.-updates/ s#^//#  #' /tmp/50unattended-upgra
 [ "$CFG" == "/etc/apt/apt.conf.d/50unattended-upgrades.ucf-old" ] && mv $CFG /etc/apt/apt.conf.d/50unattended-upgrades.ucf-save
 
 ## phpmyadmin
-if [ "$(dpkg -l | grep -Ec '^i.*phpmyadmin ')" = "1" ]; then wget https://gist.githubusercontent.com/waja/77e3d2febb0745478466344f0ce5a50e/raw/deploy_phpmyadmin_buster.sh -O /tmp/a && sh /tmp/a; fi && \
 if [ -f /etc/phpmyadmin/config.inc.php.dpkg-new ]; then CFG=/etc/phpmyadmin/config.inc.php.dpkg-new; \
    else CFG=/etc/phpmyadmin/config.inc.php; fi
 sed -i "s/\['auth_type'\] = 'cookie'/\['auth_type'\] = 'http'/" $CFG
@@ -107,16 +103,16 @@ sed -i "s/^#Port 22/Port 1234/" /etc/ssh/sshd_config && /etc/init.d/ssh restart
 # full-upgrade
 apt-get dist-upgrade
 
-# Migrate (webserver) from php7.0 to php7.3
-apt install $(dpkg -l |grep php7.0 | awk '/^i/ { print $2 }' |grep -v ^php7.0-opcache |sed s/php7.0/php/)
-[ -L /etc/apache2/mods-enabled/mpm_prefork.load ] && a2dismod php7.0 && a2enmod php7.3 && systemctl restart apache2; ls -la /etc/php/7.0/*/conf.d/
+# Migrate (webserver) from php7.3 to php7.4
+apt install $(dpkg -l |grep php7.3 | awk '/^i/ { print $2 }' |grep -v ^php7.3-opcache |sed s/php7.3/php/)
+[ -L /etc/apache2/mods-enabled/mpm_prefork.load ] && a2dismod php7.3 && a2enmod php7.4 && systemctl restart apache2; ls -la /etc/php/7.3/*/conf.d/
 # php-fpm
-tail -10 /etc/php/7.0/fpm/pool.d/www.conf
-vi /etc/php/7.3/fpm/pool.d/www.conf 
-systemctl disable php7.0-fpm && systemctl stop php7.0-fpm && systemctl restart php7.3-fpm
+tail -10 /etc/php/7.3/fpm/pool.d/www.conf
+vi /etc/php/7.4/fpm/pool.d/www.conf 
+systemctl disable php7.3-fpm && systemctl stop php7.3-fpm && systemctl restart php7.4-fpm
 # nginx
-rename s/php70/php73/g /etc/nginx/conf.d/*php70*.conf
-sed -i s/php7.0-fpm/php7.3-fpm/g /etc/nginx/conf.d/*.conf /etc/nginx/snippets/*.conf /etc/nginx/sites-available/*
+rename s/php73/php74/g /etc/nginx/conf.d/*php73*.conf
+sed -i s/php7.3-fpm/php7.4-fpm/g /etc/nginx/conf.d/*.conf /etc/nginx/snippets/*.conf /etc/nginx/sites-available/*
 systemctl restart nginx
 
 # Update old postfix configurations
@@ -126,21 +122,11 @@ if [ -z $(postconf -nh compatibility_level) ]; then sed -iE 's/^readme_directory
 diff -Nur /tmp/postfix/main.cf /etc/postfix/main.cf && \
 postfix reload
 
-# transition docker-ce to buster package
-DOCKER_VER="$(apt-cache policy docker-ce | grep debian-buster | head -1 | awk '{print $1}')" && [ -n "${DOCKER_VER}" ] && apt install docker-ce=${DOCKER_VER} docker-ce-cli=${DOCKER_VER}
+# transition docker-ce to bullseye package
+DOCKER_VER="$(apt-cache policy docker-ce | grep debian-bullseye | head -1 | awk '{print $1}')" && [ -n "${DOCKER_VER}" ] && apt install docker-ce=${DOCKER_VER} docker-ce-cli=${DOCKER_VER}
 
-# transition icingaweb2 to buster package
-ICINGAWEB2_VER="$(apt-cache policy icingaweb2 | grep "\.buster" | head -1 | awk '{print $1}')" && [ -n "${ICINGAWEB2_VER}" ] && apt install icingaweb2=${ICINGAWEB2_VER} icingaweb2-common=${ICINGAWEB2_VER} icingaweb2-module-monitoring=${ICINGAWEB2_VER} php-icinga=${ICINGAWEB2_VER} icingacli=${ICINGAWEB2_VER}
-
-# provide /etc/dovecot/private/dovecot.key from default config to prevent failing to start dovecot
-ln -s /etc/ssl/private/ssl-cert-snakeoil.key /etc/dovecot/private/dovecot.key
-
-# Fixing systemd unit for chrooted unbound (https://bugs.debian.org/931583)
-mkdir -p /etc/systemd/system/unbound.service.d/ && cat > /etc/systemd/system/unbound.service.d/override.conf <<EOF
-[Service]
-BindPaths=/run/systemd/notify:/var/lib/unbound/run/systemd/notify
-EOF
-systemctl daemon-reload && systemctl restart unbound
+# transition icingaweb2 to bullseye package
+ICINGAWEB2_VER="$(apt-cache policy icingaweb2 | grep "\.bullseye" | head -1 | awk '{print $1}')" && [ -n "${ICINGAWEB2_VER}" ] && apt install icingaweb2=${ICINGAWEB2_VER} icingaweb2-common=${ICINGAWEB2_VER} icingaweb2-module-monitoring=${ICINGAWEB2_VER} php-icinga=${ICINGAWEB2_VER} icingacli=${ICINGAWEB2_VER}
 
 # remove old squeeze packages left around (keep eyes open!)
 apt autoremove && \
@@ -152,6 +138,7 @@ apt purge $(dpkg -l | grep -E 'deb6|squeeze' | grep -v xen | awk '/^rc/ { print 
 apt purge $(dpkg -l | grep -E 'deb7|wheezy' | grep -v xen | grep -v  -E 'linux-image|mailscanner|openswan|debian-security-support' | awk '/^rc/ { print $2 }') && \
 apt purge $(dpkg -l | grep -E 'deb8|jessie' | grep -v xen | grep -v  -E 'linux-image|debian-security-support' | awk '{ print $2 }') && \
 apt purge $(dpkg -l | grep -E 'deb9|stretch' | grep -v xen | grep -v  -E 'linux-image|debian-security-support|icinga2|phpmyadmin' | awk '{ print $2 }') && \
+apt purge $(dpkg -l | grep -E 'deb10|buster' | grep -v xen | grep -v  -E 'linux-image|debian-security-support|icinga2|phpmyadmin' | awk '{ print $2 }') && \
 apt -y install deborphan && apt purge $(deborphan | grep -v xen | grep -v -E 'libpam-cracklib|libapache2-mpm-itk')
 apt purge $(dpkg -l | awk '/^rc/ { print $2 }')
 
@@ -160,7 +147,7 @@ reboot && sleep 180; echo u > /proc/sysrq-trigger ; sleep 2 ; echo s > /proc/sys
 
 ### not needed until now
 # Upgrade postgres
-# See also https://www.debian.org/releases/stretch/amd64/release-notes/ch-information.de.html#plperl
+# See also https://www.debian.org/releases/buster/amd64/release-notes/ch-information.de.html#plperl
 if [ "$(dpkg -l | grep "postgresql-9.4" | awk {'print $2'})" = "postgresql-9.4" ]; then \
  aptitude install postgresql-9.6 && \
  pg_dropcluster --stop 9.6 main && \
